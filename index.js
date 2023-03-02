a3c72f66e29e7ec79041c0ffe43be5be3e82ec57
Nikhil-Kherajani <nikhilkherajani7@gmail.com>
2023-03-02 14:46:48 +0530

    server

---
+++ index.js
@@ -0,0 +1,53 @@
+const http = require("http");
+const express = require("express");
+const cors = require("cors");
+const socketIO = require("socket.io");
+
+const app = express();
+const port = process.env.PORT;
+
+const users = [{}];
+
+app.use(cors());
+app.get("/", (req, res) => {
+  res.send("HELL ITS WORKING");
+});
+
+const server = http.createServer(app);
+
+const io = socketIO(server);
+
+io.on("connection", (socket) => {
+  console.log("New Connection");
+
+  socket.on("joined", ({ user }) => {
+    users[socket.id] = user;
+    console.log(`${user} has joined `);
+    socket.broadcast.emit("userJoined", {
+      user: "Admin",
+      message: `${users[socket.id]} has joined`,
+    });
+    socket.emit("welcome", {
+      user: "Admin",
+      message: `Welcome to the chat, ${
+        users[socket.id]
+      } please use formal language only and don't use any abusive language :)`,
+    });
+  });
+
+  socket.on("message", ({ message, id }) => {
+    io.emit("sendMessage", { user: users[id], message, id });
+  });
+
+  socket.on("disconnect", () => {
+    socket.broadcast.emit("leave", {
+      user: "Admin",
+      message: `${users[socket.id]}  has left`,
+    });
+    console.log(`${users[socket.id]} has left`);
+  });
+});
+
+server.listen(port, () => {
+  console.log(`Server is working on http://localhost:${port}`);
+});
